17079b1d445447e3436fa388499c0a15c6123781
Dan Lizotte <dlizotte@gmail.com>
2021-03-13 13:09:51 -0500

    Improved script that shrinks emojis.

--- scripts/convert_to_matrixportal.sh
+++ scripts/convert_to_matrixportal.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # This command omits all national flags, Fitzpatrick Scale, and male/female gender modifiers
 # There isn't enough space to do them all on 2MB flash. 
 # You could easily copy a few on from each if needed, 
@@ -6,4 +7,12 @@
 # When the matrixportal searches for an emoji, it first looks for the full filename (all codepoints)
 # but then backs off to just the part before the first hyphen (first codepoint)
 # which is usually a "default" version of the emoji.
-ls -1 *.png | egrep -v -e '-1f1|-1f3f[b-f]|-264[02]-' | sed 's/.png//g' | xargs -I{} magick {}.png -gamma 0.76 -background black -alpha remove -alpha off -adaptive-resize 21x21 -colors 16 -type Palette BMP:bmps/{}.bmp
+
+fileconvert () {
+    magick $1.png -adaptive-resize 21x21 -gamma 0.55 -dither None -colors 16 - | magick - -background black -alpha remove -alpha off -colors 16 -type Palette BMP:bmps/$1.bmp
+}
+
+for FNAME in `ls -1 *.png | egrep -v -e '-1f1|-1f3f[b-f]|-264[02]-' | sed 's/.png//g'`
+do
+    fileconvert $FNAME
+done
